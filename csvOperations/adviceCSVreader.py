272f66ce0ab2f548e348f73d0033d99a8b945db2
fdoroszkiewicz226142 <fdoroszkiewicz226142@e-science.pl>
2018-05-27 22:15:40 +0200

    changess

--- csvOperations/adviceCSVreader.py
+++ csvOperations/adviceCSVreader.py
@@ -1,8 +1,11 @@
 import csv
+
+
 class Comment(object):
     def __init__(self, author, text):
         self.author = author
         self.text = text
+
 
 class Advice(object):
     def __init__(self, title, autor, rate, text, comments ):
@@ -11,6 +14,18 @@ class Advice(object):
         self.rate = rate
         self.text = text
         self.comments = comments
+
+    def printing(self):
+
+        print(self.title, "\t\t | Rate:", "%.2f" % self.rate,
+              "\t\t | Comments", self.comments.__len__())
+
+    def rated(self, rating, comm):
+
+        self.rate = ((self.rate*self.comments.__len__())+rating)\
+                    / (self.comments.__len__() + 1)
+        self.comments.append(comm)
+
 
 def readAdvices(adviceList, fileName):
     with open(fileName, 'r') as csvfile:
