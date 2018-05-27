39b2c5065844b74cd5555a26993ac16ae2b92ed9
fdoroszkiewicz226142 <fdoroszkiewicz226142@e-science.pl>
2018-05-27 22:18:18 +0200

    changess

--- adviceAdder/advices.py
+++ adviceAdder/advices.py
@@ -1,5 +1,27 @@
 import sys
 
+class Advice(object):
+
+
+    def __init__(self, title, autor, rate, text, comments ):
+        self.title = title
+        self.autor = autor
+        self.rate = rate
+        self.text = text
+        self.comments = comments
+
+    def printing(self):
+
+        print(self.title, "\t\t | Rate:", "%.2f" % self.rate,
+              "\t\t | Comments", self.comments.__len__())
+
+    def rated(self, rating, comm):
+
+        self.rate = ((self.rate*self.comments.__len__())+rating)\
+                    / (self.comments.__len__() + 1)
+        self.comments.append(comm)
+
+
 
 def add_advice(adviceList):
     advice_author = input("Author name: ")
