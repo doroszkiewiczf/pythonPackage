21bd2e85d1042288b6bd69632312bcb8a56f6dab
fdoroszkiewicz226142 <fdoroszkiewicz226142@e-science.pl>
2018-05-27 20:43:05 +0200

    changess

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AdviceLib',
-    version='0.2',
+    version='1.1',
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='Example advice python package',
