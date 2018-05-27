2545f1c1061901b2ed382bbc03a1211a3add04c8
fdoroszkiewicz226142 <fdoroszkiewicz226142@e-science.pl>
2018-05-27 20:13:16 +0200

    setup change

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='Example advice python package',
-    install_requires=['csv'],
     url='https://github.com/doroszkiewiczf/pythonPackage',
     author='Filip Doroszkiewicz',
     author_email='doroszkiewicz.f@gmail.com'
